3aa496acd640b9784a58c1f6e4cfe29ffaee776f
kaspavo4 <kaspavo4@fel.cvut.cz>
2025-02-13 20:11:51 +0100

    fix

--- script/DBClient.js
+++ script/DBClient.js
@@ -1,69 +1,10 @@
-const { Client } = require('pg');
-const bcrypt = require('bcrypt');
+const { Pool } = require("pg");
 
-class DBClient {
-  constructor() {
-    var connectionString = "postgres://userName:password@serverName/ip:port/nameOfDatabase";
-    this.client = new pg.Client(connectionString);
-    console.log("xdddd")
-  }
+const connectionString =
+"postgresql://postgres:ylFwcBwcNfkjRHTMIpVuHDImqbxKxbDH@autorack.proxy.rlwy.net:58239/railway";
 
-  // Připojení k databázi
-  async connect() {
-    try {
-      await this.client.connect();
-      console.log('Připojeno k databázi');
-    } catch (error) {
-      console.error('Chyba při připojování k databázi:', error);
-      throw error;  // Hodíme chybu dál, aby byla zachycena v hlavní logice
-    }
-  }
+const pool = new Pool({
+  connectionString,
+});
 
-  // Vložení uživatele (s hashováním hesla)
-  async insertUser(username, password) {
-    try {
-      // Hashování hesla
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Vložení dat do tabulky users
-      await this.client.query(
-        'INSERT INTO users (username, password) VALUES ($1, $2)', 
-        [username, hashedPassword]
-      );
-      console.log('Uživatel registrován!');
-    } catch (error) {
-      console.error('Chyba při registraci uživatele:', error);
-      throw error;  // Hodíme chybu dál
-    }
-  }
-
-  // Zavření připojení k databázi
-  async close() {
-    try {
-      await this.client.end();
-      console.log('Připojení k databázi uzavřeno');
-    } catch (error) {
-      console.error('Chyba při uzavírání připojení:', error);
-    }
-  }
-}
-
-// Příklad použití třídy DBClient pro registraci uživatele
-(async () => {
-  const dbClient = new DBClient();
-  
-  try {
-    // Připojení k databázi
-    await dbClient.connect();
-
-    // Registrace uživatele
-    await dbClient.insertUser('uzivatel1', 'tajneheslo');
-  } catch (error) {
-    console.error('Chyba v aplikaci:', error);
-  } finally {
-    // Uzavření připojení
-    await dbClient.close();
-  }
-})();
-
-new DBClient();
+module.exports = pool;
---
+++ script/test.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const pool = require("./DBClient");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/posts", async (req, res) => {
+  const posts = await pool.query("SELECT * FROM posts;");
+  res.send({ posts });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
